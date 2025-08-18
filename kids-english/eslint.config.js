c6e0c46f533b66250f75c4948e575499fe1b2656
dalong981 <chenleyi521@gmail.com>
2025-08-18 11:26:15 +0800

    chore: fix ci to use npm and add deps

--- kids-english/eslint.config.js
+++ kids-english/eslint.config.js
@@ -1,16 +1,7 @@
 import js from '@eslint/js';
-import react from 'eslint-plugin-react';
-import tsParser from '@typescript-eslint/parser';
-import tsPlugin from '@typescript-eslint/eslint-plugin';
+import tseslint from 'typescript-eslint';
 
-export default [
+export default tseslint.config(
   js.configs.recommended,
-  {
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: { parser: tsParser },
-    plugins: { '@typescript-eslint': tsPlugin, react },
-    rules: {
-      ...react.configs.recommended.rules,
-    }
-  }
-];
+  ...tseslint.configs.recommended
+);
